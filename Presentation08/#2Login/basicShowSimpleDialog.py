1ac4754132ac55d15f3473e81cf82dcd6adfa445
Rune Langøy <rune.langoy@gmail.com>
2024-10-07 14:22:30 +0200

    fixup

--- Presentation08/#2Login/basicShowSimpleDialog.py
+++ Presentation08/#2Login/basicShowSimpleDialog.py
@@ -1,17 +1,15 @@
 from PyQt6.QtWidgets import QPlainTextEdit,QFileDialog,QLabel,QPushButton,QCheckBox,QDial,QMessageBox
 from PyQt6.QtGui import QIcon, QAction,QPixmap,QDropEvent
 from PyQt6.QtWidgets import QApplication,QMainWindow
-
-
-
+from PyQt6 import uic,QtGui
 
 # Run the program
 if __name__ == '__main__':
     # You need one (and only one) QApplication instance per application.
     app = QApplication([])
-
-    window = QMainWindow()   # Create QtMainWindow
-    window.show()
+    #dialog=uic.loadUi('loginDialog.ui')
+    dialog = uic.loadUi('SimpleLoginDialog.ui')
+    dialog.show()
 
     # Start the event loop.
     app.exec()               # Wait for program to be closed
