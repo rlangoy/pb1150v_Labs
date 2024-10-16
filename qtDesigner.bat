13e70727511e61b8c3f1bb810352a5ec7b260bd4
Rune Langøy <rune.langoy@gmail.com>
2024-10-16 10:57:31 +0200

    Check and install matplotlib if needed

--- qtDesigner.bat
+++ qtDesigner.bat
@@ -4,6 +4,7 @@ set "folderPath=..\dist"
 
 :: python -m pip install pyserial
 
+echo check if 'pyserial' is installed
 python -c "import pkgutil; exit(0 if pkgutil.find_loader('serial') else 1)"
 if errorlevel 1 (
     echo The Python module 'pyserial' is not installed.
@@ -21,6 +22,27 @@ if errorlevel 1 (
     echo The Python module 'pyserial' is already installed.
 )
 
+echo check if 'matplotlib' is installed
+
+python -c "import pkgutil; exit(0 if pkgutil.find_loader('matplotlib') else 1)"
+if errorlevel 1 (
+    echo The Python module 'matplotlib' is not installed.
+    echo Installing 'matplotlib'...
+    python -m pip install matplotlib
+    python -c "import pkgutil; exit(0 if pkgutil.find_loader('matplotlib') else 1)"
+
+    if errorlevel  1 (
+        echo Failed to install the 'matplotlib' module.
+        exit /b 1
+    ) else (
+        echo The 'matplotlib' module was successfully installed.
+    )
+) else (
+    echo The Python module 'matplotlib' is already installed.
+)
+
+
+
 echo Check if pyqt6-tools is installed
 pyqt6-tools.exe  >nul 2>&1
 Echo Errorlevel: [%errorlevel%]
@@ -28,12 +50,6 @@ if %errorlevel% neq 0 (
     echo installing Qt Designer
 
     python -m pip install pyqt6-tools
-    echo Check if pyqt6-tools was properly installed
-    pyqt6-tools.exe  >nul 2>&1
-    '' Echo Errorlevel: [%errorlevel%]
-    if %errorlevel% neq 0 (
-    echo Error installing pyqt6-tools
-    exit /b 1
     )
 )
 
